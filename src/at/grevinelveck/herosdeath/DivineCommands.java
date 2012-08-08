4f68e81aa6bb9860a6b5d11bf2fd510bd1475e60
Stefan Wright <egoboater@aol.com>
2012-08-08 07:37:24 -0400

    Working on fixing adding information to config

--- src/at/grevinelveck/herosdeath/DivineCommands.java
+++ src/at/grevinelveck/herosdeath/DivineCommands.java
@@ -1,12 +1,14 @@
 package at.grevinelveck.herosdeath;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 public class DivineCommands implements CommandExecutor {
+	public HerosDeath plugin;
 
 	public boolean onCommand(CommandSender sender, Command command, String label,
 			String[] args) {
@@ -15,11 +17,21 @@ public class DivineCommands implements CommandExecutor {
 			if (sender.hasPermission("HerosDeath.Revive")) {
 				if (args.length == 0) {
 					player = Bukkit.getPlayer(sender.getName());
+					player.sendMessage(ChatColor.GOLD +"You were revived");
+					plugin.getConfig().getStringList("names")
+					.add(player.getPlayer().getName() + ", alive");
+			plugin.saveConfig();
 				} else {
 					player = Bukkit.getPlayer(args[0]);
 				}
 				if (player == null) {
-					sender.sendMessage("Must have a name to revive a player");
+					sender.sendMessage(ChatColor.RED +"Must have a name to revive a player");
+				}else{
+					player.sendMessage(ChatColor.GOLD +"You were revived");
+					plugin.getConfig().getStringList("names")
+					.add(player.getPlayer().getName() + ", alive");
+			plugin.saveConfig();
+					
 				}
 			}
 		}
@@ -37,7 +49,7 @@ public class DivineCommands implements CommandExecutor {
 			if (sender.hasPermission("HerosDeath.Haunt")) {
 				player = Bukkit.getPlayer(args[0]);
 			} else {
-				sender.sendMessage("Player not found");
+				sender.sendMessage(ChatColor.RED +"Player not found");
 
 			}
 		}
